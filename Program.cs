92e11040c304c5db2c60ee88d58077f91039b29e
Adi438 <mishraaaditya54@gmail.com>
2022-06-01 22:44:23 +0530

    bubble sort code

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Day16
+namespace BubbleSortProgram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n\n-----------------Welcome to BubbleSort Program------------\n\n");
+            Console.Write("Please enter the length of the Array: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Please enter the array elements : ");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            BubbleSort.BubbleSorting(arr);
         }
     }
 }
