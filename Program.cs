8806d38f7a581b46e9e4ba9f6f6b4fee24d930f5
Adi438 <mishraaaditya54@gmail.com>
2022-06-01 22:46:57 +0530

    Insertion sort

--- Program.cs
+++ Program.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 
-namespace BubbleSortProgram
+namespace InsertionSort
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n\n-----------------Welcome to BubbleSort Program------------\n\n");
-            Console.Write("Please enter the length of the Array: ");
-            int n = int.Parse(Console.ReadLine());
-
-            int[] arr = new int[n];
-
-            Console.WriteLine("Please enter the array elements : ");
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("\n\n------------WELCOME TO THE INSERTION SORTING PROGRAM----------------\n\n");
+            Console.Write("Enter the length of array : ");
+            int length = int.Parse(Console.ReadLine());
+            int[] arr = new int[length];
+            Console.WriteLine("Enter the element of array :");
+            for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = int.Parse(Console.ReadLine());
             }
 
-            BubbleSort.BubbleSorting(arr);
+            InsertionSort.sort(arr);
         }
     }
 }
